2097509c712d9024192251a4f72ab4e8b209dee4
Jake Levine <jake@agileramen.com>
2016-10-02 14:24:24 -0700

    Add call for json via https

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,13 @@
 // Import libraries
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 // Create component
 class AlbumList extends Component {
   componentWillMount() {
-    console.log('componentWillMount in AlbumList');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response));
   }
 
   render() {
