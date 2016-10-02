bbc527e4ce46731787d2371110122e19812b3831
Jake Levine <jake@agileramen.com>
2016-10-02 16:33:21 -0700

    Finish up albums app

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,26 +1,48 @@
 // Import libraries
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 // Create component
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title,
+          artist,
+          thumbnail_image,
+          image,
+          url
+  } = album;
+  const { headerContentStyles,
+          thumbnailStyle,
+          thumbnailContainerStyle,
+          headerTextStyle,
+          imageStyle
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             source={{ uri: thumbnail_image }}
-            style={styles.thumbnailStyle}
-            />
+            style={thumbnailStyle}
+          />
         </View>
-        <View style={styles.headerContentStyles}>
-          <Text>{title}</Text>
+        <View style={headerContentStyles}>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now!
+        </Button>
       </CardSection>
     </Card>
   );
@@ -31,9 +53,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
